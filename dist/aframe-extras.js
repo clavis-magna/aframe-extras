a61f7f1b957eb6a2c47dd7b402d2265892beb12f
andrew burrell <andrew@miscellanea.com>
2017-09-27 18:23:40 +1000

    Update aframe-extras.js

--- dist/aframe-extras.js
+++ dist/aframe-extras.js
@@ -23039,8 +23039,8 @@ module.exports = {
     return new THREE.Vector2(
       (gamepad.buttons[GamepadButton.DPAD_RIGHT].pressed ? 1 : 0)
       + (gamepad.buttons[GamepadButton.DPAD_LEFT].pressed ? -1 : 0),
-      (gamepad.buttons[GamepadButton.DPAD_UP].pressed ? 1 : 0)
-      + (gamepad.buttons[GamepadButton.DPAD_DOWN].pressed ? -1 : 0)
+      (gamepad.buttons[GamepadButton.DPAD_UP].pressed ? -1 : 0)
+      + (gamepad.buttons[GamepadButton.DPAD_DOWN].pressed ? 1 : 0)
     );
   },
 
